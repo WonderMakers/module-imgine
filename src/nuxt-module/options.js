a83840f139115af76af60145323bfb5987a66c6b
Сергей Савин <serzz1990@gmail.com>
2021-10-27 12:44:09 +0000

    Update options.js

--- src/nuxt-module/options.js
+++ src/nuxt-module/options.js
@@ -2,5 +2,6 @@ module.exports = {
   namespace: 'imgine.space',
   cacheLimit: 60,
   endpoint: 'https://imgine.space/c/',
-  staticEndpoint: 'https://host/'
+  staticEndpoint: 'https://host/',
+  useLoader: false
 }
